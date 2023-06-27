e316bc23d03338ca5ed167bf9fcf4ff4b0250a90
Danillo Wilian <danwillgon@gmail.com>
2023-06-27 09:59:13 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { GlobalStyle } from "./styles/GlobalStyle"
+function App() {
+
+  return (
+    <>
+    <GlobalStyle />
+    <h1>Hello React teste 1</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/styles/GlobalStyle.ts
@@ -0,0 +1,18 @@
+import { createGlobalStyle } from "styled-components"
+
+export const GlobalStyle = createGlobalStyle`
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+body {
+    height: 100hv;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    justify-content: center;
+    background-color: yellow;
+}
+`;
